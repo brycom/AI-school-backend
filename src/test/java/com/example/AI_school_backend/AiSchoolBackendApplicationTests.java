7c915444eb39bc5b82fb09a07528d90ef76be257
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-10-15 11:00:15 +0200

    Start

---
+++ src/test/java/com/example/AI_school_backend/AiSchoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AI_school_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSchoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
